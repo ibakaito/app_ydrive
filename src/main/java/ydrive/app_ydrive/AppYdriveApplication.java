989ea99263b4d3006f6bcbf5cf70f58dc44314c7
B191019 <e1b19019@st.oit.ac.jp>
2021-10-26 16:14:29 +0900

    SSEの作成

--- src/main/java/ydrive/app_ydrive/AppYdriveApplication.java
+++ src/main/java/ydrive/app_ydrive/AppYdriveApplication.java
@@ -2,8 +2,10 @@ package ydrive.app_ydrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AppYdriveApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ydrive/app_ydrive/controller/Sample51Controller.java
+++ src/main/java/ydrive/app_ydrive/controller/Sample51Controller.java
@@ -21,18 +21,55 @@ import ydrive.app_ydrive.model.FruitMapper;
 @RequestMapping("/sample5")
 public class Sample51Controller {
 
-  // @Autowired
-  // FruitMapper fMapper;
+  @Autowired
+  FruitMapper fMapper;
 
   @GetMapping("step1")
   public String sample51() {
     return "sample51.html";
   }
 
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
   @GetMapping("step3")
   @Transactional
   public String sample53(@RequestParam Integer id, ModelMap model) {
-    // 削除対象のフルーツを取得
     Fruit fruit3 = fMapper.selectById(id);
     model.addAttribute("fruit3", fruit3);
+    fMapper.deleteById(id);
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  @GetMapping("step4")
+  @Transactional
+  public String sample54(@RequestParam Integer id, ModelMap model) {
+    Fruit fruit4 = fMapper.selectById(id);
+    model.addAttribute("fruit4", fruit4);
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  @PostMapping("step5")
+  public String sample55(@RequestParam Integer id, @RequestParam String name, @RequestParam Integer price,
+      ModelMap model) {
+    System.out.println("step5");
+    System.out.println(id);
+    System.out.println(name);
+    System.out.println(price);
+    Fruit fruit = new Fruit(id, name, price);
+    fMapper.updateById(fruit);
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+
+    return "sample51.html";
+  }
+
 }
