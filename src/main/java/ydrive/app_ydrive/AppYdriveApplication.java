41d7bebbb4308edd8690895ec5d4938a075e4495
ibakaito <e1b19009@oit.ac.jp>
2021-10-26 14:03:41 +0900

    リポジトリの初期化

---
+++ src/main/java/ydrive/app_ydrive/AppYdriveApplication.java
@@ -0,0 +1,13 @@
+package ydrive.app_ydrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppYdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppYdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ydrive/app_ydrive/AppYdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package ydrive.app_ydrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppYdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
